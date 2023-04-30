91f79f3b87d427f19f4d2076939bbf47001e2f5c
Sabrina-Maria <98300850+sabrisakka@users.noreply.github.com>
2023-04-30 13:43:47 +0400

    Add files via upload

---
+++ stapp.py
@@ -0,0 +1,298 @@
+import time
+# Initialize prediction_time variable
+prediction_time = 0
+# Make prediction when button is clicked
+start_time = time.time()  # Start timer
+import streamlit as st
+import tensorflow as tf
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from sklearn.model_selection import train_test_split
+import pandas as pd
+import unicodedata
+import re
+
+import numpy as np
+import pandas as pd
+import os
+import io
+import time
+import pickle
+from sklearn.model_selection import train_test_split
+import string
+from string import digits
+import re
+from sklearn.utils import shuffle
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.layers import LSTM, Input, Dense,Embedding, Concatenate, TimeDistributed
+from tensorflow.keras.models import Model,load_model, model_from_json
+from tensorflow.keras.utils import plot_model
+from tensorflow.keras.preprocessing.text import one_hot, Tokenizer
+from tensorflow.keras.callbacks import EarlyStopping
+from keras import backend as K 
+import pickle as pkl
+import numpy as np
+
+import tensorflow as tf
+from tensorflow.python.keras import backend as K
+
+logger = tf.get_logger()
+
+class AttentionLayer(tf.keras.layers.Layer):
+    """
+    This class implements Bahdanau attention (https://arxiv.org/pdf/1409.0473.pdf).
+    There are three sets of weights introduced W_a, U_a, and V_a
+     """
+
+    def __init__(self, **kwargs):
+        super(AttentionLayer, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+        assert isinstance(input_shape, list)
+        # Create a trainable weight variable for this layer.
+
+        self.W_a = self.add_weight(name='W_a',
+                                   shape=tf.TensorShape((input_shape[0][2], input_shape[0][2])),
+                                   initializer='uniform',
+                                   trainable=True)
+        self.U_a = self.add_weight(name='U_a',
+                                   shape=tf.TensorShape((input_shape[1][2], input_shape[0][2])),
+                                   initializer='uniform',
+                                   trainable=True)
+        self.V_a = self.add_weight(name='V_a',
+                                   shape=tf.TensorShape((input_shape[0][2], 1)),
+                                   initializer='uniform',
+                                   trainable=True)
+
+        super(AttentionLayer, self).build(input_shape)  # Be sure to call this at the end
+
+    def call(self, inputs):
+        """
+        inputs: [encoder_output_sequence, decoder_output_sequence]
+        """
+        assert type(inputs) == list
+        encoder_out_seq, decoder_out_seq = inputs
+
+        logger.debug(f"encoder_out_seq.shape = {encoder_out_seq.shape}")
+        logger.debug(f"decoder_out_seq.shape = {decoder_out_seq.shape}")
+
+        def energy_step(inputs, states):
+            """ Step function for computing energy for a single decoder state
+            inputs: (batchsize * 1 * de_in_dim)
+            states: (batchsize * 1 * de_latent_dim)
+            """
+
+            logger.debug("Running energy computation step")
+
+            if not isinstance(states, (list, tuple)):
+                raise TypeError(f"States must be an iterable. Got {states} of type {type(states)}")
+
+            encoder_full_seq = states[-1]
+
+            """ Computing S.Wa where S=[s0, s1, ..., si]"""
+            # <= batch size * en_seq_len * latent_dim
+            W_a_dot_s = K.dot(encoder_full_seq, self.W_a)
+
+            """ Computing hj.Ua """
+            U_a_dot_h = K.expand_dims(K.dot(inputs, self.U_a), 1)  # <= batch_size, 1, latent_dim
+
+            logger.debug(f"U_a_dot_h.shape = {U_a_dot_h.shape}")
+
+            """ tanh(S.Wa + hj.Ua) """
+            # <= batch_size*en_seq_len, latent_dim
+            Ws_plus_Uh = K.tanh(W_a_dot_s + U_a_dot_h)
+
+            logger.debug(f"Ws_plus_Uh.shape = {Ws_plus_Uh.shape}")
+
+            """ softmax(va.tanh(S.Wa + hj.Ua)) """
+            # <= batch_size, en_seq_len
+            e_i = K.squeeze(K.dot(Ws_plus_Uh, self.V_a), axis=-1)
+            # <= batch_size, en_seq_len
+            e_i = K.softmax(e_i)
+
+            logger.debug(f"ei.shape = {e_i.shape}")
+
+            return e_i, [e_i]
+
+        def context_step(inputs, states):
+            """ Step function for computing ci using ei """
+
+            logger.debug("Running attention vector computation step")
+
+            if not isinstance(states, (list, tuple)):
+                raise TypeError(f"States must be an iterable. Got {states} of type {type(states)}")
+
+            encoder_full_seq = states[-1]
+
+            # <= batch_size, hidden_size
+            c_i = K.sum(encoder_full_seq * K.expand_dims(inputs, -1), axis=1)
+
+            logger.debug(f"ci.shape = {c_i.shape}")
+
+            return c_i, [c_i]
+
+        # we don't maintain states between steps when computing attention
+        # attention is stateless, so we're passing a fake state for RNN step function
+        fake_state_c = K.sum(encoder_out_seq, axis=1)
+        fake_state_e = K.sum(encoder_out_seq, axis=2)  # <= (batch_size, enc_seq_len, latent_dim
+
+        """ Computing energy outputs """
+        # e_outputs => (batch_size, de_seq_len, en_seq_len)
+        last_out, e_outputs, _ = K.rnn(
+            energy_step, decoder_out_seq, [fake_state_e], constants=[encoder_out_seq]
+        )
+
+        """ Computing context vectors """
+        last_out, c_outputs, _ = K.rnn(
+            context_step, e_outputs, [fake_state_c], constants=[encoder_out_seq]
+        )
+
+        return c_outputs, e_outputs
+
+    def compute_output_shape(self, input_shape):
+        """ Outputs produced by the layer """
+        return [
+            tf.TensorShape((input_shape[1][0], input_shape[1][1], input_shape[1][2])),
+            tf.TensorShape((input_shape[1][0], input_shape[1][1], input_shape[0][1]))
+        ]
+json_file = open('Spell_model.json', 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+model_loaded = model_from_json(loaded_model_json, custom_objects={'AttentionLayer': AttentionLayer})
+# load weights into new model
+model_loaded.load_weights("Spell_model_weight.h5")
+
+with open('inputTokenizer.pkl', 'rb') as f:
+    inputTokenizer = pickle.load(f)
+
+with open('outputTokenizer.pkl', 'rb') as f:
+    outputTokenizer = pickle.load(f)
+
+Eindex2word = inputTokenizer.index_word
+Mindex2word = outputTokenizer.index_word
+
+latent_dim=300
+# encoder inference
+encoder_inputs = model_loaded.input[0]  #loading encoder_inputs
+encoder_outputs, state_h, state_c = model_loaded.layers[4].output #loading encoder_outputs
+
+print(encoder_outputs.shape)
+
+encoder_model = Model(inputs=encoder_inputs,outputs=[encoder_outputs, state_h, state_c])
+
+# decoder inference
+# Below tensors will hold the states of the previous time step
+decoder_state_input_h = Input(shape=(latent_dim,), name="decoder_state_input_h")
+decoder_state_input_c = Input(shape=(latent_dim,), name="decoder_state_input_c")
+decoder_hidden_state_input = Input(shape=(34,latent_dim))
+
+# Get the embeddings of the decoder sequence
+decoder_inputs = model_loaded.layers[1].output
+
+print(decoder_inputs.shape)
+dec_emb_layer = model_loaded.layers[3]
+
+dec_emb2= dec_emb_layer(decoder_inputs)
+
+# To predict the next word in the sequence, set the initial states to the states from the previous time step
+decoder_lstm = model_loaded.layers[5]
+decoder_outputs2, state_h2, state_c2 = decoder_lstm(dec_emb2, initial_state=[decoder_state_input_h, decoder_state_input_c])
+
+#attention inference
+attn_layer = model_loaded.layers[6]
+attn_out_inf, attn_states_inf = attn_layer([decoder_hidden_state_input, decoder_outputs2])
+
+concate = model_loaded.layers[7]
+decoder_inf_concat = concate([decoder_outputs2, attn_out_inf])
+
+# A dense softmax layer to generate prob dist. over the target vocabulary
+decoder_dense = model_loaded.layers[8]
+decoder_outputs2 = decoder_dense(decoder_inf_concat)
+
+# Final decoder model
+decoder_model = Model(
+[decoder_inputs] + [decoder_hidden_state_input,decoder_state_input_h, decoder_state_input_c],
+[decoder_outputs2] + [state_h2, state_c2])
+
+
+def decode_sequence(input_seq):
+    # Encode the input as state vectors.
+    e_out, e_h, e_c = encoder_model.predict(input_seq, verbose=0)
+
+    # Generate empty target sequence of length 1.
+    target_seq = np.zeros((1,1))
+
+    # Chose the 'start' word as the first word of the target sequence
+    target_seq[0, 0] = Mword2index['<']
+
+    stop_condition = False
+    decoded_sentence = ''
+    while not stop_condition:
+        output_tokens, h, c = decoder_model.predict([target_seq] + [e_out, e_h, e_c], verbose=0)
+
+        # Sample a token
+        sampled_token_index = np.argmax(output_tokens[0, -1, :])
+        if sampled_token_index == 0:
+          break
+        else:
+          sampled_token = Mindex2word[sampled_token_index]
+
+          if(sampled_token!='>'):
+              decoded_sentence += ''+sampled_token
+
+              # Exit condition: either hit max length or find stop word.
+              if (sampled_token == '>' or len(decoded_sentence.split()) >= (26-1)):
+                  stop_condition = True
+
+          # Update the target sequence (of length 1).
+          target_seq = np.zeros((1,1))
+          target_seq[0, 0] = sampled_token_index
+
+          # Update internal states
+          e_h, e_c = h, c
+
+    return decoded_sentence
+def seq2summary(input_seq):
+    newString=''
+    for i in input_seq:
+      if((i!=0 and i!=Mword2index['<']) and i!=Mword2index['>']):
+        newString=newString+Mindex2word[i]+''
+    return newString
+
+def seq2text(input_seq):
+    newString=''
+    for i in input_seq:
+      if(i!=0):
+        newString=newString+Eindex2word[i]+''
+    return newString
+
+Mword2index = outputTokenizer.word_index
+
+
+# Define function to make predictions
+#def predict(sentence):
+#    # Replace with your prediction code here
+#    return sentence.upper()
+
+# Set page title and icon
+st.set_page_config(page_title="Spelling Correction Model", page_icon=":pencil2:")
+# Define app layout
+st.write("<h1>Spelling Correction Model</h1>", unsafe_allow_html=True)
+st.write("<p>Enter a sentence:</p>", unsafe_allow_html=True)
+sentence = st.text_input("")
+
+
+if st.button("Predict"):
+    if sentence:
+        
+        new_sample = ["< " + sentence +" >"]
+        new_sample = np.array(pad_sequences(inputTokenizer.texts_to_sequences(new_sample), maxlen=34, padding='post'))
+        
+        prediction = decode_sequence(new_sample.reshape(1, 34))
+        
+        st.write(f"Prediction: {prediction}")
+        end_time = time.time()  # End timer
+        prediction_time = end_time - start_time
+        st.write(f"Prediction time: {prediction_time:.2f} seconds")
+    else:
+        st.write("Please enter a sentence.")
